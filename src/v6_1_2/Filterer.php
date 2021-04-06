c80ff5cbd778b033c19b3a8383f1a8e2c223255b
PiotrPress <admin@piotr.press>
2021-04-06 10:34:15 +0200

    v6.1.2

--- src/v6_1_2/Filterer.php
+++ src/v6_1_2/Filterer.php
@@ -1,14 +1,14 @@
 <?php
 
 /*
-    Copyright (C) 2020 by Clearcode <https://clearcode.cc>
+    Copyright (C) 2021 by Clearcode <https://clearcode.cc>
     and associates (see AUTHORS.txt file).
 
     This file is part of clearcode/wordpress-framework.
 
     clearcode/wordpress-framework is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
+    the Free Software Foundation; either version 3 of the License, or
     (at your option) any later version.
 
     clearcode/wordpress-framework is distributed in the hope that it will be useful,
@@ -21,7 +21,7 @@
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 
-namespace Clearcode\Framework\v6_1_1;
+namespace Clearcode\Framework\v6_1_2;
 
 use ReflectionClass;
 use ReflectionMethod;
